0c1681b4ecd1b49fd3989b6aefc71d8486b6cc40
Shaun Geer <scgeer@gmail.com>
2026-07-23 20:51:11 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE posts (
     post_id    TEXT PRIMARY KEY,
     title      TEXT, 
     parent_id  TEXT REFERENCES posts(post_id),
-    -- user_id is NULL when the author was deleted, because Reddit no longer exposes who wrote it.
+    -- user_id is NULL when the author deletes their account.
     user_id    TEXT REFERENCES users(user_id),
     body_text  TEXT NOT NULL,
     flair   TEXT,
